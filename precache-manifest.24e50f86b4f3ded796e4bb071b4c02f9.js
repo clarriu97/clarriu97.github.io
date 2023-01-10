f2287726c3ad4d2c70d21cd763838c3d45bb78e6
Carlos Larriu <larriucarlos@gmail.com>
2023-01-10 23:08:43 +0100

    Updates

---
+++ precache-manifest.24e50f86b4f3ded796e4bb071b4c02f9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00395200f3da441707e8e1e0c4f57303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac2ea34168f5c244f19",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "9377fec8f9934e8cb5d1",
+    "url": "/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "113ea348b5fcf78c699e",
+    "url": "/static/css/main.1a6005f5.chunk.css"
+  },
+  {
+    "revision": "081570e72aa26c8a48b8",
+    "url": "/static/js/2.83417a77.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.83417a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac2ea34168f5c244f19",
+    "url": "/static/js/3.199ba6a9.chunk.js"
+  },
+  {
+    "revision": "9377fec8f9934e8cb5d1",
+    "url": "/static/js/4.d6d855cb.chunk.js"
+  },
+  {
+    "revision": "113ea348b5fcf78c699e",
+    "url": "/static/js/main.c8880e13.chunk.js"
+  },
+  {
+    "revision": "594e37704c00163bff39",
+    "url": "/static/js/runtime-main.993813eb.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "5431da43683f9ae62150afb60957a8a4",
+    "url": "/static/media/mowLogo.5431da43.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "0ed32d05a28824a93b68478bd581e821",
+    "url": "/static/media/upnaLogo.0ed32d05.png"
+  },
+  {
+    "revision": "372756a9968ee6ba07267993a5a4a71a",
+    "url": "/static/media/veridasLogo.372756a9.png"
+  },
+  {
+    "revision": "8c3ccbdec1e3e64fce75bea19aa63a2c",
+    "url": "/static/media/zuastiLogo.8c3ccbde.png"
+  }
+]);
